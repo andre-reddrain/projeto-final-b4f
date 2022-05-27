b9501d46d21e5f041f0df251fda7d3fe715efb67
André Rosa <andre.reddrain@gmail.com>
2022-05-27 11:25:02 +0100

    React Routes.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Login from './components/Login'
+import App from './App';
+import Login from './Pages/Login'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Login />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
