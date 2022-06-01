a6b8cbeedb980e88707bcf2034e7e2bf8eeca7f8
André Rosa <andre.reddrain@gmail.com>
2022-06-01 16:36:22 +0100

    FontAwesome Icons + Styling of Catalog. Changes on CSS.

--- frontend/src/Pages/Catalog.jsx
+++ frontend/src/Pages/Catalog.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { secondsToMinutes, secondsToHours } from "../components/validate"
+import styles from "../styles/style.module.css"
+
+// FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Catalog() {
     const token = (localStorage.getItem("token") !== "null") ?
@@ -51,6 +56,16 @@ export default function Catalog() {
         return json
     }
 
+    function handleClick(_id) {
+        // console.log("Clicou-me! " + _id)
+        return navigate(`/catalog/content/${String(_id)}`)
+    }
+
+    function filter(e) {
+        console.log(e.target.value)
+        setCatalog(catalog.filter(content => String(content.name).includes(e.target.value) == true))
+    }
+
     return (
         <div>
             <h1>CATALOGGGGGGG</h1>
@@ -66,33 +81,34 @@ export default function Catalog() {
                     <a href={`/login`}>Login</a>
             }
             <br />
+            <input type="text" placeholder="a tua mae" onChange={(e) => filter(e)}></input>
             {
                 catalog.map((content) => (
-                    <div
-                        key={content._id}
-                    >
+
+                    <div key={content._id}>
                         {
                             content.type === 0 ?
-                                <div>
-                                    <h2>Serie</h2>
-                                    <a href={`/catalog/content/${String(content._id)}`}>Nome: {content.name}</a>
+                                <div className={styles.divseries} onClick={() => handleClick(content._id)}>
+                                    <div className={styles.divImage} style={{ backgroundImage: "url('https://cdn.vox-cdn.com/thumbor/aIgnekvyjdARf_NVJj21EL37uT8=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23434598/DrStrange2_Payoff_1_Sht_v6_Lg.jpeg')" }}>
+                                    </div>
+                                    <div style={{ marginTop: "15vh", padding: "5px", paddingTop: "10px", textAlign: "left" }}>
+                                        {content.name}<br />({content.releaseDate.substring(0, 4)})
+                                        <div style={{ float: "right", marginBottom: "5px" }}>
+                                            <FontAwesomeIcon icon={faHeart} style={{ color: "red" }}></FontAwesomeIcon> {Math.floor(Math.random() * (100 - 50 + 1)) + 50}%
+                                        </div>
+                                    </div>
                                 </div>
                                 :
-                                <div>
-                                    <h2>Filme</h2>
-                                    <a href={`/catalog/content/${String(content._id)}`}>Titulo: {content.title}</a>
-                                </div>
+                                <p>Epah ya</p>
+                            // <div>
+                            //     <h2>Filme</h2>
+                            //     <a href={`/catalog/content/${String(content._id)}`}>Titulo: {content.title}</a>
+                            // </div>
                         }
-                        <p>Tempo: {secondsToHours(content.seconds)}</p>
-                        {
-                            content.image ?
-                                <img src={content.image[0]} alt="imagem" width="400" height="600" /> :
-                                <img src="future.webp" alt="imagem" width="400" height="600" />
-                        }
-                        <p>Data de lançamento: {content.releaseDate}</p>
+                        <br></br>
                     </div>
                 ))
             }
-        </div>
+        </div >
     )
 }
